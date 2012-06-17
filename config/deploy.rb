7101bdb275f91bd25690f3c5f1ac843ec76f1692
Yang tao <emcse@163.com>
2012-06-18 01:57:05 +0800

    hh

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,17 +20,17 @@ set :deploy_env, "development"
 set :rails_env, "development"
 set :normalize_asset_timestamps, false
 
-namespace :deploy do
-  desc "cause Passenger to initiate a restart"
-  task :restart do
-    run "touch #{current_path}/tmp/restart.txt" 
-  end
-  
-  desc "reload the database with seed data"
-  task :seed do
-    run "cd #{current_path}; rake db:seed RAILS_ENV=development"
-  end
-end
+# namespace :deploy do
+#   desc "cause Passenger to initiate a restart"
+#   task :restart do
+#     run "touch #{current_path}/tmp/restart.txt" 
+#   end
+#   
+#   desc "reload the database with seed data"
+#   task :seed do
+#     run "cd #{current_path}; rake db:seed RAILS_ENV=development"
+#   end
+# end
 
 # after "deploy:update_code", :bundle_install
 # desc "install the necessary prerequisites"
